6d99d92d755225ee18b19202028cc7a004490c9c
pixlblade <79928968+pixlblade@users.noreply.github.com>
2021-05-02 21:14:55 -0400

    Transistors and Processors
    Added transistors, hammers, iron plates, and processor chips

--- src/main/java/net/pixlblade/storange/registry/ModItems.java
+++ src/main/java/net/pixlblade/storange/registry/ModItems.java
@@ -5,17 +5,24 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-import org.lwjgl.system.CallbackI;
 import net.pixlblade.storange.storange;
 
 public class ModItems {
     // Items
     public static final Item ORANGE = new Item(new Item.Settings().group(storange.STORANGE_GROUP));
+    public static final Item TRANSISTOR = new Item(new Item.Settings().group(storange.STORANGE_GROUP));
+    public static final Item IRON_PLATE = new Item(new Item.Settings().group(storange.STORANGE_GROUP));
+    public static final Item HAMMER = new Item(new Item.Settings().group(storange.STORANGE_GROUP));
+    public static final Item PROCESSOR_CHIP = new Item(new Item.Settings().group(storange.STORANGE_GROUP));
     // Block Items
     public static final BlockItem MACHINE_BLOCK = new BlockItem(ModBlocks.MACHINE_BLOCK, new Item.Settings().group(storange.STORANGE_GROUP));
     // Registry
     public static void registerItems(){
         Registry.register(Registry.ITEM, new Identifier(storange.MOD_ID, "orange"), ORANGE);
         Registry.register(Registry.ITEM, new Identifier(storange.MOD_ID, "machine_block"), MACHINE_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(storange.MOD_ID, "transistor"), TRANSISTOR);
+        Registry.register(Registry.ITEM, new Identifier(storange.MOD_ID, "iron_plate"), IRON_PLATE);
+        Registry.register(Registry.ITEM, new Identifier(storange.MOD_ID, "hammer"), HAMMER);
+        Registry.register(Registry.ITEM, new Identifier(storange.MOD_ID, "processor_chip"), PROCESSOR_CHIP);
     }
 }
